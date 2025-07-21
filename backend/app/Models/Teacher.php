a3d45eae7a11c0e7dc43e76a6edc40ec8e5279a9
Vikram Singh <vikramsingh.vks06@gmail.com>
2025-07-21 14:09:15 +0530

    teacher model

--- backend/app/Models/Teacher.php
+++ backend/app/Models/Teacher.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Teacher extends Model
 {
     use HasFactory;
-	protected $table =''
+	protected $table ='teacher';
     /**
      * The attributes that are mass assignable.
      *
