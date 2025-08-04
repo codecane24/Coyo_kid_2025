8e80b92d343aeecba5c0e42961d0fb1d92c09785
Vikram Singh <vikramsingh.vks06@gmail.com>
2025-08-04 17:57:45 +0530

    fdsafdsa

--- frontend/src/services/AdmissionInquiry.ts
+++ frontend/src/services/AdmissionInquiry.ts
@@ -2,26 +2,26 @@
 import axiosInstance from "../utils/axiosInstance";
 
 export const getInquiryById = async (id: string) => {
-  const response = await axiosInstance.get(`/inquiry/${id}`);
+  const response = await axiosInstance.get(`/admission-inquiry/${id}`);
   return response.data;
 };
 
 export const getInquiryList = async () => {
-  const response = await axiosInstance.get("/inquiry");
+  const response = await axiosInstance.get("/admission-inquiry");
   return response.data;
 };
 
 export const createInquiry = async (inquiryData: any) => {
-  const response = await axiosInstance.post("/inquiry", inquiryData);
+  const response = await axiosInstance.post("/admission-inquiry", inquiryData);
   return response.data;
 };
 
 export const updateInquiry = async (id: string, inquiryData: any) => {
-  const response = await axiosInstance.put(`/inquiry/${id}`, inquiryData);
+  const response = await axiosInstance.put(`/admission-inquiry/${id}`, inquiryData);
   return response.data;
 };
 
 export const deleteInquiry = async (id: string) => {
-  const response = await axiosInstance.delete(`/inquiry/${id}`);
+  const response = await axiosInstance.delete(`/admission-inquiry/${id}`);
   return response.data;
 };
